a933b3a6deeaf4c6de99bfba6bc9343a119bf33c
mytory <mytory@gmail.com>
2013-09-18 18:16:36 +0900

    언어 지원 버그

--- main.php
+++ main.php
@@ -8,7 +8,11 @@ Author URI: http://mytory.net
 */
 
 function aoil_script(){
-	wp_enqueue_script( 'add-original-image-link', plugin_dir_url(__FILE__) . get_bloginfo('language') . '.js', array('jquery'), '1.0', 1 );
+    $lang = 'en';
+    if(get_bloginfo('language') == 'ko-KR'){
+        $lang = 'ko-KR';
+    }
+	wp_enqueue_script( 'add-original-image-link', plugin_dir_url(__FILE__) . $lang . '.js', array('jquery'), '1.0', 1 );
 }
 
 add_action('wp_enqueue_scripts', 'aoil_script');
