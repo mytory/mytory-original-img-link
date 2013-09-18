211033df8deca97ac5a4bf58fdd2672ee126cce1
mytory <mytory@gmail.com>
2013-09-18 18:08:10 +0900

    readme.md 추가. 윈도우 리사이즈하면 얼마가 확대될지 갱신해줌.

--- js.js
+++ js.js
@@ -1,26 +1,39 @@
 jQuery(document).ready(function($){
+
+    var add_original_img_link = function(img){
+        var scale_ratio = img.clientWidth / img.naturalWidth;
+        if(scale_ratio <= 0.8){
+            var original_img_link__a = $('<a />', {
+                href: img.src,
+                text: ' ▷이미지 ' + Math.round(1 / scale_ratio * 10) / 10 + '배 확대',
+                'class': 'original-img-link__a',
+                title: '현재 이 이미지는 ' + Math.round(scale_ratio * 100) + '%로 축소된 상태입니다.',
+                target: '_blank'
+            });
+            var container = $('<span />', {
+                'class': 'original-img-link'
+            });
+            container.append(original_img_link__a);
+
+            if($(img).next().is('.wp-caption-text')){
+                $(img).next().append(container);
+            }else{
+                $(img).after(container);
+            }
+        }
+    };
+
     $('.content img').each(function(){
         $(this).load(function(){
-            var scale_ratio = this.clientWidth / this.naturalWidth;
-            if(scale_ratio <= 0.8){
-                var original_img_link__a = $('<a />', {
-                    href: this.src,
-                    text: ' ▷이미지 ' + Math.round(1 / scale_ratio * 10) / 10 + '배 확대',
-                    'class': 'original-img-link__a',
-                    title: '현재 이 이미지는 ' + Math.round(scale_ratio * 100) + '%로 축소된 상태입니다.',
-                    target: '_blank'
-                });
-                var container = $('<span />', {
-                    'class': 'original-img-link'
-                });
-                container.append(original_img_link__a);
-
-                if($(this).next().is('.wp-caption-text')){
-                    $(this).next().append(container);
-                }else{
-                    $(this).after(container);
-                }
-            }
+            add_original_img_link(this);
+        });
+    });
+    $(window).resize(function(){
+        if($('.original-img-link').length > 0){
+            $('.original-img-link').remove();
+        }
+        $('.content img').each(function(){
+            add_original_img_link(this);
         });
     });
 });
---
+++ readme.md
@@ -0,0 +1,6 @@
+Mytory Original IMG Link
+========================
+
+My blog `img` `max-width` is `100%`. On wide screen, that's right. But on narrow screen, this is so small. So I made a plugin.
+
+This plugin add link to original img.
